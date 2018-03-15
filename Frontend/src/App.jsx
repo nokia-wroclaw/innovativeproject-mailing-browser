3ed902a8856be37e1d2a754ef1b63ed125c2debc
Jakub Woszczyna <kubwosz@gmail.com>
2018-03-15 17:49:20 +0100

    Podstawowa wersja z navbarami

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,51 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Menu } from 'semantic-ui-react'
+import SideMenu from './Menu';
+import MenuExampleSecondaryPointing from './MenuPointing';
+
 
 class App extends Component {
-  state = {}
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
   render() {
-    const { activeItem } = this.state
     return (
       <div>
-        <Menu>
-        <Menu.Item
-          name='editorials'
-          active={activeItem === 'editorials'}
-          onClick={this.handleItemClick}
-        >
-          Editorials
-        </Menu.Item>
-
-        <Menu.Item
-          name='reviews'
-          active={activeItem === 'reviews'}
-          onClick={this.handleItemClick}
-        >
-          Reviews
-        </Menu.Item>
-
-        <Menu.Item
-          name='upcomingEvents'
-          active={activeItem === 'upcomingEvents'}
-          onClick={this.handleItemClick}
-        >
-          Upcoming Events
-        </Menu.Item>
-      </Menu>
-
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-    </div>
+        <div>
+        <MenuExampleSecondaryPointing/>
+        </div>
+        <div>
+        <SideMenu/>
+        </div>
     </div>
     );
 
