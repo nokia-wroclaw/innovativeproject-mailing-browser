151e32adae5375ee0e669ab6cefca72af353886b
paulm96 <pawi_xu@interia.pl>
2018-04-23 18:45:55 +0200

    searching in threads and answers in one routing

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -7,46 +7,60 @@ var Thread = MyThread.Thread
 const Sequelize = require('sequelize')
 const Op = Sequelize.Op
 
-router.get('/thread/:searchPhrase', function (req, res, next) {
+router.get('/:searchPhrase', function (req, res, next) {
     Thread.findAll({
             where: {
                     [Op.or]: [
                         { Subject : { [Op.iLike]: '%'+req.params.searchPhrase+'%' }},
-                        { Text: { [Op.iLike]: '%'+req.params.searchPhrase+'%' }}
-                      //  { '$Mail.Text': { [Op.iLike]: '%'+req.params.searchPhrase+'%' }}
+                        { Text: { [Op.iLike]: '%'+req.params.searchPhrase+'%' }}                     
                     ]
                 },
             order: [
                 ['Date', 'ASC'],
             ]
         }).then((thread) => {
-            res.json(thread);
-        });        
+            var threadlist=[];
+                    for(i = 0; i < thread.length; i++) {
+                        threadlist.push(thread[i].messageId);
+                    }
+            Mail.findAll({
+                where: {
+                    [Op.or]: [
+                        { Subject : { [Op.iLike]: '%'+req.params.searchPhrase+'%' }},
+                        { Text: { [Op.iLike]: '%'+req.params.searchPhrase+'%' }}                
+                    ]
+                }
+            }).then((answer)=>{
+                var list=[];
+                var check = false;
+                    for(i = 0; i < answer.length; i++) {
+                        for(j = 0; j < threadlist.length; j++) {
+                            if(answer[i].reference == threadlist[j]) {
+                                check = true;
+                                break;
+                            }
+                        }
+                        if(check == false) list.push(answer[i].reference);
+                        check = false;
+                    }
+                if(list.length == 0) {
+                    res.json(thread);
+                    res.end();
+                } else {
+                    Thread.findAll({
+                    where: {
+                        messageId: {
+                            [Op.or]: list
+                          }
+                    }
+                    }).then((thread2)=>{
+                        res.json([thread,...thread2]);
+                        res.end();
+                    })
+                }          
+            })    
+        });    
 });
 
-router.get('/answer/:searchPhrase', function (req, res, next) {
-    Mail.findAll({
-        where: {
-            [Op.or]: [
-                { Subject : { [Op.iLike]: '%'+req.params.searchPhrase+'%' }},
-                { Text: { [Op.iLike]: '%'+req.params.searchPhrase+'%' }}                
-            ]
-        }
-    }).then((answer)=>{
-        var list=[];
-            for(i = 0; i < answer.length; i++) {
-                list.push(answer[i].reference);
-            }
-        Thread.findAll({
-        where: {
-            messageId: {
-                [Op.or]: list
-              }
-        }
-        }).then((thread)=>{
-            res.json(thread);
-            res.end();
-        })          
-    })
-});
+
 module.exports = router;
