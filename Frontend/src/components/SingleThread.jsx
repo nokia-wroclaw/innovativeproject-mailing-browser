e38a5a11340cceeb99edb1bf6864ce6c87406c52
Jakub Woszczyna <kubwosz@gmail.com>
2018-04-24 11:34:18 +0200

    Fixed datetime

--- Frontend/src/components/SingleThread.jsx
+++ Frontend/src/components/SingleThread.jsx
@@ -128,7 +128,7 @@ export default class SingleThread extends Component {
                         <Header as='h2'>{this.state.mails ? this.state.mails[0].Subject : null}</Header>
                     </Segment>
                     <Segment tertiary>
-                        <Header as='h5'> Wysłane: {moment(this.state.mails ? this.state.mails[0].Date : null).format('DD/MM/YYYY, h:mm:ss')}</Header>
+                        <Header as='h5'> Wysłane: {moment(this.state.mails ? this.state.mails[0].Date : null).format('DD/MM/YYYY, HH:mm:ss')}</Header>
                         Od: {this.state.mails ? this.state.mails[0].From : null} <br/>
                         Do: {this.state.mails ? this.state.mails[0].To : null}
                     </Segment>
